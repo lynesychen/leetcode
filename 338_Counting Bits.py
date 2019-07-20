c4d8d00e49ab62fc9c179ae5913a1560c1058e78
lynesychen <lynesychen@hotmail.com>
2019-07-20 10:40:06 +0800

    bit

--- 338_Counting Bits.py
+++ 338_Counting Bits.py
@@ -1,10 +1,6 @@
 class Solution:
     def countBits(self, num: int) -> List[int]:
         res = [0]
-        j = 1
         for i in range(1, num + 1):
-            if i - j == j:
-                j *= 2
-            res.append(res[i - j] + 1)
-            
+            res.append(res[i>>1] + (i & 1))
         return res

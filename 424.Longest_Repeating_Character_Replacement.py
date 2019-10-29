b19f10fa503c367a41e04fe0bbef5915fc686aab
lynesychen <lynesychen@hotmail.com>
2019-10-29 15:40:39 +0800

    滑动窗口

--- 424.Longest_Repeating_Character_Replacement.py
+++ 424.Longest_Repeating_Character_Replacement.py
@@ -1,18 +1,20 @@
-ass Solution:
+# 事实证明defaultdict还是比自己造数组快了四倍
+from collections import defaultdict
+
+class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         if s == '':
             return 0
-        count = [0] * 26
+        count = defaultdict(int)
         max_len = 0
         width = k + 1
         st_idx = 0
         lens = len(s)
-        orda = ord('A')
         max_cnt = 0
         max_char = ""
         for c in s[:width]:
-            tmp = count[ord(c)-orda]
-            count[ord(c)-orda] = tmp + 1
+            tmp = count[c]
+            count[c] = tmp + 1
             if tmp + 1 > max_cnt:
                 max_cnt = tmp + 1
                 max_char = c
@@ -20,8 +22,8 @@ ass Solution:
             if width - max_cnt <= k:
                 max_len = max(max_len, width)
                 if st_idx + width < lens:
-                    tmp = count[ord(s[st_idx+width]) - orda]
-                    count[ord(s[st_idx+width]) - orda] = tmp + 1
+                    tmp = count[s[st_idx+width]]
+                    count[s[st_idx+width]] = tmp + 1
                     if tmp + 1 >= max_cnt:
                         max_cnt = tmp + 1
                         max_char = s[st_idx+width]
@@ -29,12 +31,11 @@ ass Solution:
                 else:
                     break
             else:
-                tmp = count[ord(s[st_idx])-orda]
-                count[ord(s[st_idx])-orda] = tmp - 1
+                tmp = count[s[st_idx]]
+                count[s[st_idx]] = tmp - 1
                 if s[st_idx] == max_char:
                     max_cnt -= 1
                 st_idx += 1
                 width -= 1
         return max_len
-
 
